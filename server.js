98334e1a655f29feae7440dfd29694b25e1d13cf
jackie <jcqnly@gmail.com>
2018-04-14 11:11:51 -0700

    successful connection to heroku

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 const conString = 'postgres://localhost:5432/books_app';
 const client = new pg.Client(conString);
 
-app.get('/test', (req, res) => {
-  res.send('testing');
-});
+// app.get('/test', (req, res) => {
+//   res.send('testing');
+// });
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
