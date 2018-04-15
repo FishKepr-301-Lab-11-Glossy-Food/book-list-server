ccded84990f5e6619838d7214a4baef7772cd53e
Phillip Kim <phildkim@gmail.com>
2018-04-15 15:15:48 -0700

    fixed formatting a bit

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const pg = require('pg');
 const cors = require('cors');
 const fs = require('fs');
 const express = require('express');
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT;
 const app = express();
 const CLIENT_URL = process.env.CLIENT_URL;
 
@@ -12,6 +13,7 @@ const CLIENT_URL = process.env.CLIENT_URL;
 const DATABASE_URL = 'postgres://urksfufglaniux:45984851e5ec00418d245fe521c165749e81cb3825c276ddf0197853c5aa11b2@ec2-54-243-54-6.compute-1.amazonaws.com:5432/dd0s6294njttog';
 const client = new pg.Client(DATABASE_URL);
 client.connect();
+
 app.use(cors());
 
 app.get('*', (req, res) => res.redirect(CLIENT_URL));
